cb8ff8978c6f8fd435005a0bd844c4c702a8d42f
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-10-27 21:17:20 +0000

    model schema defaults expenses to 0 on signup since it's dynamically summed based on account transactions

--- src/controller/auth/signup.controller.ts
+++ src/controller/auth/signup.controller.ts
@@ -1,10 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { Response, Request } from "express";
 import bcryptjs from "bcryptjs";
-import * as data from '../../../data.json'
+import * as data from "../../../data.json";
 const prisma = new PrismaClient();
 
-export type controller = (  req: Request, res:Response)=>void
+export type controller = (req: Request, res: Response) => void;
 
 type reqBody = {
   email: string;
@@ -16,42 +16,46 @@ type reqBody = {
   balance: number;
 };
 
-export const Register:controller = async (req, res) => {
-  const { email, name, password, income, avatar, expenses, balance }: reqBody =
+export const Register: controller = async (req, res) => {
+  const { email, name, password, income, avatar, balance }: reqBody =
     req.body;
   const salt = await bcryptjs.genSalt(10);
   const hashed = await bcryptjs.hash(password, salt);
 
   const user = await prisma.user.create({
-    data: {email, income, avatar, name, password: hashed, expenses, balance}
+    data: { email, income, avatar, name, password: hashed, balance },
   });
 
-  if(!user.id){return res.end('User not created, please try again')}
+  if (!user.id) {
+    return res.end("User not created, please try again");
+  }
 
-  const placeHolderTransactions = data.transactions.map(item => {
-    item.userId = user.id
-    return item
-  })
+  const placeHolderTransactions = data.transactions.map((item) => {
+    item.userId = user.id;
+    return item;
+  });
 
-  const placeHolderBudgets = data.budgets.map(item => {
-    item.userId = user.id
-    return item
-  })
+  const placeHolderBudgets = data.budgets.map((item) => {
+    item.userId = user.id;
+    return item;
+  });
 
-  const placeHolderPots = data.pots.map(item => {
-    item.userId = user.id
-    return item
-  })
+  const placeHolderPots = data.pots.map((item) => {
+    item.userId = user.id;
+    return item;
+  });
 
-  const placeHolderBills = data.recurringBills.map(item => {
-    item.userId = user.id
-    return item
-  })
+  const placeHolderBills = data.recurringBills.map((item) => {
+    item.userId = user.id;
+    return item;
+  });
 
-  await prisma.transactions.createMany({data: placeHolderTransactions});
+  await prisma.transactions.createMany({ data: placeHolderTransactions });
   await prisma.pot.createMany({ data: placeHolderPots });
-  await prisma.budget.createMany({ data:placeHolderBudgets });
-  await prisma.bills.createMany({data: placeHolderBills})
+  await prisma.budget.createMany({ data: placeHolderBudgets });
+  await prisma.bills.createMany({ data: placeHolderBills });
 
-  return res.send('user created successfully');
+  return res
+    .status(200)
+    .json({ message: "user created successfully", success: true });
 };
