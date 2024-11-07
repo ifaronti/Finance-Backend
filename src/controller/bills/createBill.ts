b697948fc43c00644659f607df8eab9545a5aeb6
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-11-07 09:09:42 +0000

    oauth controller line 26 was supposed to compare data.email & user.email not data.id & user.id. Added a conditional to check if bill still exists in database before cron job starts else it stops.

--- src/controller/bills/createBill.ts
+++ src/controller/bills/createBill.ts
@@ -5,34 +5,35 @@ import { CronJob } from "cron";
 const prisma = new PrismaClient();
 
 export const createBills: controller = async (req, res) => {
-    const { amount, name, avatar, category, categoryId } = req.body;
-    //@ts-expect-error authentication middleware
-    const userId = req.user;
-    const dayOfMonth = Number(new Date().getDate());
-  
-    const newBill = await prisma.bills.create({
-      data: { amount, name, avatar, category, categoryId, userId: userId },
-    });
-  
-    if (!newBill.BillId) {
-      return res.end("An error occured, please try again");
-    }
-  
-    const balanceUpdateJob = CronJob.from({
-      cronTime: `0 0 ${dayOfMonth} * *`,
-      onTick: async function () {
-        await prisma.user.update({
-          where: { id: userId },
-          data: { balance: { decrement: amount } },
-        });
-  
-        await prisma.transactions.create({
-          data: { amount, name, avatar, category, categoryId, recurring: true, userId }
-        })
-      },
-      start: false,
-      timeZone: "Europe/London",
-    });
+  const { amount, name, avatar, category, categoryId } = req.body;
+  //@ts-expect-error authentication middleware
+  const userId = req.user;
+  const dayOfMonth = Number(new Date().getDate());
+  const formattedDay = Number(dayOfMonth) < 10? '0'+dayOfMonth:dayOfMonth
+
+  const newBill = await prisma.bills.create({
+    data: { amount, name, avatar, category, categoryId, userId: userId },
+  });
+
+  if (!newBill.BillId) {
+    return res.end("An error occured, please try again");
+  }
+
+  const balanceUpdateJob = CronJob.from({
+    cronTime: `0 0 ${Number(formattedDay)} * *`,
+    onTick: async function () {
+      await prisma.user.update({
+        where: { id: userId },
+        data: { balance: { decrement: amount } },
+      });
+
+      await prisma.transactions.create({
+        data: { amount, name, avatar, category, categoryId, recurring: true, userId }
+      })
+    },
+    start: false,
+    timeZone: "Europe/London",
+  });
   const checkBill = await prisma.bills.findFirst({ where: { BillId: newBill.BillId } })
   
   if (!checkBill?.BillId) {
@@ -42,5 +43,5 @@ export const createBills: controller = async (req, res) => {
     balanceUpdateJob.start()
   }
   
-    return res.end("Bill created");
-  };
+  return res.end("Bill created");
+};
